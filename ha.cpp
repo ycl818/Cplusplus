40d64bb5c4b270aac22f25771e46edb54d494265
yichien <kevinyichienlee@gmail.com>
2020-11-30 23:34:51 +0800

    exercise

---
+++ ha.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("H     H     A\n");
+    printf("H     H    A A\n");
+    printf("HHHHHHH   A   A\n");
+    printf("H     H  AAAAAAA\n");
+    printf("H     H  A     A");
+    return 0;
+}
