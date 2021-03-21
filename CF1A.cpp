298d369917a8b793f1e54c1d2a40efbcb3359871
yichien <kevinyichienlee@gmail.com>
2021-03-21 22:34:38 +0800

    Add files via upload
    CF1A

---
+++ CF1A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	int A, B;
+  	cin >> A >>  B;
+  	cout<<  (A+B-1) / B; 
+  	return 0;
+//  Y = 3
+//  X  1 2 3 4 5 6 
+// ans 1 1 1 2 2 2
+//  /  0 0 1 1 1 2
+// (X -2) / 3
+
+// induce (X + Y - 1) /Y
+}
+
