b0946fa456db8a93579651afecb54485a96f5503
yichien <kevinyichienlee@gmail.com>
2021-04-25 22:27:11 +0800

    Add files via upload

---
+++ intro_c++/6_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int i = 1; i <=19; i++){
+  	for(int j = 1; j <=19; j++){
+    	cout << i << "*" << j << "=" << i*j;
+    }
+    cout << endl;
+  }
+  return 0;
+}
