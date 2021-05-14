0dbec862225e74e4368ff83bb885c78518b2a110
yichien <kevinyichienlee@gmail.com>
2021-05-14 13:17:52 +0800

    green_judge_a037

---
+++ green_judge_a037.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  int n;
+  cin >> n;
+  while (n--) {
+    cin >> a >> b;
+    if (a > b) {
+      cout << "A"
+           << " " << a - b << endl;
+    } else if (b > a) {
+      cout << "B"
+           << " " << b - a << endl;
+    } else if (a == b) {
+      cout << 0 << endl;
+    }
+  }
+
+  return 0;
+}
