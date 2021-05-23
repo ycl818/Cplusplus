abdad5af76972ef4e1555fe99da0cbd82bb61461
yichien <kevinyichienlee@gmail.com>
2021-05-24 01:30:48 +0800

    vector_recursive_findMax

---
+++ Feis/vector_recursive_findMax.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int RangeMax(vector<int>& v, int first, int last) {
+  if (first == last) return v[first];
+  int mid = (first + last) / 2;
+  int left = RangeMax(v, first, mid);
+  int right = RangeMax(v, mid + 1, last);
+  if (left > right) {
+    return left;
+  }
+  return right;
+}
+int Max(vector<int>& v) { return RangeMax(v, 0, v.size() - 1); }
+int main() {
+  vector<int> v = {3, 5, 6, 9, 1};
+  cout << Max(v) << endl;
+  return 0;
+}
