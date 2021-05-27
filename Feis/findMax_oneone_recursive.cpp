9e0a776910e955370a10ddb8d4bfc209c3194ec6
yichien <kevinyichienlee@gmail.com>
2021-05-27 23:37:49 +0800

    Create findMax_oneone_recursive.cpp

---
+++ Feis/findMax_oneone_recursive.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Max(vector<int>& v, int size){
+  if(size == 1) return v[0];
+  int m = Max(v, size-1);
+  if(v[size-1] > m){
+    return v[size-1];
+  }
+  return m;
+}
+int Max(vector<int>& v){
+  return Max(v, v.size());
+}
+int main() {
+  vector<int> v = {3,5,7,9,1};
+  cout << Max(v) << endl;
+  return 0;
+}
