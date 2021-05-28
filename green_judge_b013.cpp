bbee08a294b17325a391877b4a6f58d7c5758802
yichien <kevinyichienlee@gmail.com>
2021-05-28 22:44:34 +0800

    green_judge_b013

---
+++ green_judge_b013.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s1, s2, s3;
+
+  getline(cin, s1);
+  getline(cin, s2);
+  getline(cin, s3);
+  cout << s3 << endl;
+  cout << s1 << endl;
+  cout << s2 << endl;
+  return 0;
+}
