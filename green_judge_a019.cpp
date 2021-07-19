6cd1a582eb831648660edceeab5a753380f30ade
yichien <kevinyichienlee@gmail.com>
2021-07-20 00:33:44 +0800

    green_judge_a019

---
+++ green_judge_a019.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+using namespace std;  
+int main() {  
+  int a, b, sum = 0;  
+  cin >> a >> b;  
+  if (b == 2 || b == 5 || b == 8) sum = 200;   
+  else if (a % 2 == 1) sum = 100;  
+  else if (a == b) sum = 50;  
+  cout << sum << endl;  
+  return 0;  
+}  
