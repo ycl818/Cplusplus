19516cc67abd1fac7799656ec84ea3d624ad0923
yichien <kevinyichienlee@gmail.com>
2021-07-24 00:17:53 +0800

    green_judge_a050

---
+++ green_judge_a050.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() { 
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      cout << j << "x" << i << "=" << setw(2)<< i*j << "  ";
+    }
+    cout << endl;
+  }
+}
